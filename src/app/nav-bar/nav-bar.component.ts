f54b5dbf09c0bf45596f60a5da52a373aca45a29
josepereza <josepereza66@gmail.com>
2020-03-24 18:37:07 +0100

    segundo commit

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,18 @@
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent  {
+
+
+  
+
+  
+ 
+
+
+}
